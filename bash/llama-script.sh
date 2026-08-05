afe3eb150112da071664e4ab073dfbdf22b06397
Blah029 <toransika@gmail.com>
2026-08-06 01:49:11 +0530

    update llama.sh - fine tune minimum batch sizes for maximum gpu utilisation

--- bash/llama-script.sh
+++ bash/llama-script.sh
@@ -8,8 +8,11 @@ Usage: $(basename "${BASH_SOURCE[0]}") [options] parameter
 
 Options:
     -h, --help                  Print help and exit
-    -f, --flag                  Example flag
-    -p, --parameter <parameter> Example named parameter
+    -v, --verbose               Enable verbose output
+    -m, --model <path>          Large language model in GGUF format
+    --mtp <path>                Multitoken prediction head in GGUF format
+    --mmproj <path>             Multimedia projector in GGUF format
+    -o, --offload <layers>      Numebr of layers to offload to gpu. Default all
 EOF
     exit
 }
@@ -23,22 +26,28 @@ die() {
 
 
 parse_arguments() {
-    verbose=false
-    model="12B"
-    quantisation="4"
+    # Defaults
+    verbosity=3
+    model="gemma-4-12B-it-Q4_0.gguf"
+    mtp="mtp-gemma-4-12B-it-Q4_0.gguf"
+    mmproj="mmproj-gemma-4-12B-it-Q8_0.gguf"
     gpu_offload="all"
     
     # Parse flags and named parameters
     while :; do
         case "${1-}" in
             -h | --help) usage;;
-            -v | --verbose) verbose=true;;
+            -v | --verbose) verbosity=5;;
             -m | --model)
                 model="${2-}"
                 shift
                 ;;
-            -q | --quant)
-                quantisation="${2-}"
+            --mtp)
+                mtp="${2-}"
+                shift
+                ;;
+            -mmproj)
+                mmproj="${2-}"
                 shift
                 ;;
             -o | --offload)
@@ -57,9 +66,10 @@ parse_arguments() {
     args=("$@")
 
     # Check for required named parameters
-    [[ -z "${model-}" ]] && die "Missing required parameter: --parameter"
-    [[ -z "${quantisation-}" ]] && die "Missing required parameter: --parameter"
-    [[ -z "${gpu_offload-}" ]] && die "Missing required parameter: --parameter"
+    [[ -z "${model-}" ]] && die "Missing required parameter: --model"
+    [[ -z "${mtp-}" ]] && die "Missing required parameter: --mtp"
+    [[ -z "${mmproj-}" ]] && die "Missing required parameter: --mmproj"
+    [[ -z "${gpu_offload-}" ]] && die "Missing required parameter: --offload"
     # Check for no. of positional parameters
     [[ ${#args[@]} -lt 0 ]] && die "Missing positional parameters. Given "${#args[@]}", expected 1"
     [[ ${#args[@]} -gt 0 ]] && die "Too many positional parameters. Given "${#args[@]}", expected 1"
@@ -68,16 +78,18 @@ parse_arguments() {
 
 
 main() {
-    model_path="applications/llama-cpp/models/gemma-4-${model}-it-Q${quantisation}_0.gguf"
-    mtp_path="applications/llama-cpp/models/mtp-gemma-4-${model}-it-Q${quantisation}_0.gguf"
+    model_path="applications/llama-cpp/models/${model}"
+    mtp_path="applications/llama-cpp/models/${mtp}"
+    mmproj_path="applications/llama-cpp/models/${mmproj}"
     llama serve \
         -m ${model_path} \
         -md ${mtp_path} \
+        -mm ${mmproj_path} \
         --spec-type draft-mtp \
         -t 12 \
         -c 0 \
-        -b 8192 \
-        -ub 4096 \
+        -b 256 \
+        -ub 128 \
         -fa on \
         -ctk q8_0 \
         -ctv q8_0 \
@@ -85,12 +97,23 @@ main() {
         -mg 0 \
         -fit on \
         -fitt 1024 \
+        -lv ${verbosity} \
         -ctkd q8_0 \
         -ctvd q8_0 \
         --temp 0.5 \
+        -td 12 \
         -ngld all \
         --host 0.0.0.0 \
         --port 8080
+    # Min. batch size for full gpu utilisation
+    #   -b 256 \
+    #   -ub 128 \
+    # Max. batch size for parsing large pdfs as images 
+    #   -b 2048 \
+    #   -ub 1024 \
+    # Max. batch size for parsing large pfds as text
+    #   -b 8192 \
+    #   -ub 4096 \
 }
 
 
