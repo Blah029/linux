84266f357dfaf2861bf64a77d6c10aa12652baf4
Blah029 <toransika@gmail.com>
2026-08-07 00:34:31 +0530

    update llama-script.sh - add auto-loading for speculative draft model and multimedia projector - add suppor for switching between vulkan and rocm

--- bash/llama-script.sh
+++ bash/llama-script.sh
@@ -9,9 +9,16 @@ Usage: $(basename "${BASH_SOURCE[0]}") [options] parameter
 Options:
     -h, --help                  Print help and exit
     -v, --verbose               Enable verbose output
-    -m, --model <path>          Large language model in GGUF format
-    --mtp <path>                Multitoken prediction head in GGUF format
-    --mmproj <path>             Multimedia projector in GGUF format
+    -g, --github                Run downloaded GitHub Vulkan release
+    -r, --rocm                  Run downloaded GitHub ROCm release
+    -m, --model <path>          Large language model in GGUF format. Default gemma-4-12B-it-Q4_0.gguf". 
+                                Or
+                                    12b     - Auto load Gemma 4 12B models. Good for large image PDFs
+                                    26b-m   - Audo load Gemma 4 26B A4B with Multi-Token Prediction 
+                                    26b-d   - Auto load Gemma 4 26B A4B with DFlash. Good for large text PDFs
+    --md <path>                 Multitoken prediction head in GGUF format. Default mtp-gemma-4-12B-it-Q4_0.gguf
+    --mm <path>                 Multimedia projector in GGUF format. Default mmproj-gemma-4-12B-it-Q8_0.gguf"
+    --st <type>                 Speculative decoding type
     -o, --offload <layers>      Numebr of layers to offload to gpu. Default all
 EOF
     exit
@@ -28,26 +35,35 @@ die() {
 parse_arguments() {
     # Defaults
     verbosity=3
-    model="gemma-4-12B-it-Q4_0.gguf"
-    mtp="mtp-gemma-4-12B-it-Q4_0.gguf"
-    mmproj="mmproj-gemma-4-12B-it-Q8_0.gguf"
+    model="gemma-4-26B-A4B-it-Q4_0.gguf"
+    draft_model="dflash-gemma-4-26B-A4B-it-Q8_0.gguf"
+    multimedia_projector="mmproj-gemma-4-26B-A4B-it-Q8_0.gguf"
+    speculative_type="draft-dflash"
     gpu_offload="all"
+    executable="llama serve"
     
     # Parse flags and named parameters
     while :; do
         case "${1-}" in
             -h | --help) usage;;
             -v | --verbose) verbosity=5;;
+            -g | --github) executable="applications/llama-cpp/binary/vulkan/llama-server";;
+            -r | --rocm) executable="applications/llama-cpp/binary/rocm/llama-server";;
             -m | --model)
                 model="${2-}"
+                autoload
                 shift
                 ;;
-            --mtp)
-                mtp="${2-}"
+            --md)
+                draft_model="${2-}"
                 shift
                 ;;
-            -mmproj)
-                mmproj="${2-}"
+            --mm)
+                multimedia_projector="${2-}"
+                shift
+                ;;
+            --st)
+                speculative_type="${2-}"
                 shift
                 ;;
             -o | --offload)
@@ -67,25 +83,49 @@ parse_arguments() {
 
     # Check for required named parameters
     [[ -z "${model-}" ]] && die "Missing required parameter: --model"
-    [[ -z "${mtp-}" ]] && die "Missing required parameter: --mtp"
-    [[ -z "${mmproj-}" ]] && die "Missing required parameter: --mmproj"
+    [[ -z "${draft_model-}" ]] && die "Missing required parameter: --md"
+    [[ -z "${multimedia_projector-}" ]] && die "Missing required parameter: --mm"
+    [[ -z "${speculative_type-}" ]] && die "Missing required parameter: --mm"
     [[ -z "${gpu_offload-}" ]] && die "Missing required parameter: --offload"
     # Check for no. of positional parameters
     [[ ${#args[@]} -lt 0 ]] && die "Missing positional parameters. Given "${#args[@]}", expected 1"
-    [[ ${#args[@]} -gt 0 ]] && die "Too many positional parameters. Given "${#args[@]}", expected 1"
+    [[ ${#args[@]} -gt 0 ]] && die "Too many positional parameters. Given "${#args[@]}", expected 1"    
+}
 
+
+autoload() {
+    case "${model}" in
+        "12b")
+            model="gemma-4-12B-it-Q4_0.gguf"
+            draft_model="mtp-gemma-4-12B-it-Q4_0.gguf"
+            multimedia_projector="mmproj-gemma-4-12B-it-Q8_0.gguf"
+            speculative_type="draft-mtp"
+            ;;  
+        "26b-m")
+            model="gemma-4-26B-A4B-it-Q4_0.gguf"
+            draft_model="mtp-gemma-4-26B-A4B-it-Q4_0.gguf"
+            multimedia_projector="mmproj-gemma-4-26B-A4B-it-Q8_0.gguf"
+            speculative_type="draft-mtp"
+            ;;  
+        "26b-d")
+            model="gemma-4-26B-A4B-it-Q4_0.gguf"
+            draft_model="draft-gemma-4-26B-A4B-it-Q8_0.gguf"
+            multimedia_projector="mmproj-gemma-4-26B-A4B-it-Q8_0.gguf"
+            speculative_type="draft-mtp"
+            ;;  
+    esac
 }
 
 
 main() {
     model_path="applications/llama-cpp/models/${model}"
-    mtp_path="applications/llama-cpp/models/${mtp}"
-    mmproj_path="applications/llama-cpp/models/${mmproj}"
-    llama serve \
+    draft_model_path="applications/llama-cpp/models/${draft_model}"
+    multimedia_projector_path="applications/llama-cpp/models/${multimedia_projector}"
+    ${executable} \
         -m ${model_path} \
-        -md ${mtp_path} \
-        -mm ${mmproj_path} \
-        --spec-type draft-mtp \
+        -md ${draft_model_path} \
+        -mm ${multimedia_projector_path} \
+        --spec-type ${speculative_type} \
         -t 12 \
         -c 0 \
         -b 1024 \
@@ -117,6 +157,7 @@ main() {
     # Max. batch size for parsing large pfds as text
     #   -b 8192 \
     #   -ub 4096 \
+    #
 }
 
 
