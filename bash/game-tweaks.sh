de8ab319d381c6eab7399c8f2e7ef6db8001dab8
Blah029 <toransika@gmail.com>
2026-09-01 02:16:39 +0530

    update game-tweaks and seaxng settings game-tweaks.sh - add undo gpu performance level
    searxng/core-config/settings.yml
    - update search engines

--- bash/game-tweaks.sh
+++ bash/game-tweaks.sh
@@ -8,6 +8,7 @@ Usage: $(basename "${BASH_SOURCE[0]}") [options]
 
 Options:
     -h, --help                  Print help and exit
+    -u, --undo                  Undo changes
 EOF
     exit
 }
@@ -21,10 +22,14 @@ die() {
 
 
 parse_arguments() {
+    # Defaults
+    undo_flag=false
+
     # Parse flags and named parameters
     while :; do
         case "${1-}" in
             -h | --help) usage;;
+            -u | --undo) undo_flag=true;;
             # Exit if an unexpected option is passed
             -?*) die "Unexpected option: $1";;
             # If no matches, break while loop to parse positional parameters
@@ -39,15 +44,20 @@ parse_arguments() {
 
 
 main() {
-    echo -e "\nEnabling CPU power monitoring..."
-    sudo chmod o+r /sys/class/powercap/intel-rapl\:0/energy_uj
-    echo -e "\nSetting GPU performace level to high..."
-    echo high | sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level
-    #echo -e "\nFixing sample rate and buffer quantum..."
-    #pw-metadata -n settings 0 clock.force-rate 48000
-    #pw-metadata -n settings 0 clock.force-quantum 500
-    #echo -e "\nDecreasing swappiness..."
-    #sudo sysctl vm.swappiness=10
+    if [ $undo_flag == false ]; then
+        echo -e "\nEnabling CPU power monitoring..."
+        sudo chmod o+r /sys/class/powercap/intel-rapl\:0/energy_uj
+        echo -e "\nSetting GPU performace level to high..."
+        echo high | sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level
+        #echo -e "\nFixing sample rate and buffer quantum..."
+        #pw-metadata -n settings 0 clock.force-rate 48000
+        #pw-metadata -n settings 0 clock.force-quantum 500
+        #echo -e "\nDecreasing swappiness..."
+        #sudo sysctl vm.swappiness=10
+    else
+        echo -e "\nSetting GPU performace level to auto..."
+        echo auto | sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level
+    fi
 }
 
 
