7298419be766f8bedef0cc0a283577352da4dd74
Blah029 <toransika@gmail.com>
2026-09-03 00:00:28 +0530

    update llama-script, config/ctxpact/, anythingllm/docker-compose.yaml llama-script.sh - kill existing background processes before running tools
    ctxpact
    - enable handoff
    
    anythingllm
    - add anythingllm github repository to file system tool

--- bash/llama-script.sh
+++ bash/llama-script.sh
@@ -10,6 +10,7 @@ Options:
     -h, --help                  Print help and exit
     --llama-help                Print llama.cpp help and exit
     -v, --verbose               Enable verbose output
+    -r, --restart               Kill existing processes before running command
     -a, --all                   Run all tools for AnythingLLM
     -s, --source <repository>   Binary source. Default github
                                     github          - Run downloaded GitHub Vulkan release
@@ -35,8 +36,9 @@ parse_arguments() {
     # Defaults
     llama_help_flag=false
     verbose_flag=false
-    tools_flag=false
-    executable_source="huggingface"
+    restart_flag=false
+    all_flag=false
+    command_source="huggingface"
     model="qwen-27b-r"
     embedding_model="nomic/nomic-embed-text-v1.f16.gguf"
     
@@ -46,9 +48,10 @@ parse_arguments() {
             -h | --help) usage;;
             --llama-help) llama_help_flag=true;;
             -v | --verbose) verbose_flag=true;;
-            -a | --all) tools_flag=true;;
+            -r | --restart) restart_flag=true;;
+            -a | --all) all_flag=true;;
             -s | --source)
-                executable_source="${2-}"
+                command_source="${2-}"
                 shift
                 ;;
             -m | --model)
@@ -71,16 +74,16 @@ parse_arguments() {
     args=("$@")
 
     # Check for required named parameters
+    [[ -z "${command_source-}" ]] && die "Missing required parameter: --source"
     [[ -z "${model-}" ]] && die "Missing required parameter: --model"
-    [[ -z "${embedding_model-}" ]] && die "Missing required parameter: --em"
 }
 
 
 autoload() {
-    # Executable
-    case ${executable_source} in
-        "huggingface") executable="llama serve";;
-        "github") executable="$HOME/applications/llama-cpp/binary/vulkan/llama-server";;
+    # command
+    case ${command_source} in
+        "huggingface") command="llama serve";;
+        "github") command="$HOME/applications/llama-cpp/binary/vulkan/llama-server";;
     esac
 
     # Model
@@ -112,7 +115,7 @@ autoload() {
             draft_model="gemma/unsloth/mtp-gemma-4-26B-A4B-it-qat-Q8_0.gguf"
             multimedia_projector="gemma/unsloth/mmproj-gemma-4-26B-A4B-it-qat-F16.gguf"
             speculative_type="draft-mtp"
-            executable_args+=(
+            command_args+=(
                 ${gemma_args[@]}
                 -a "gemma-4-26B-A4B-it-qat-UD-Q4_K_XL"
             )
@@ -121,7 +124,7 @@ autoload() {
             llm="qwen/empero-ai/Qwen3.8-27B-Ridge-3.7bpw.gguf"
             multimedia_projector="qwen/empero-ai/mmproj-Qwen3.8-27B-BF16.gguf"
             speculative_type="ngram-mod,draft-mtp"
-            executable_args+=(
+            command_args+=(
                 ${qwen_args[@]}
                 -a "Qwen3.8-27B-Ridge-3.7bpw"
                 -c 122880
@@ -133,7 +136,7 @@ autoload() {
         "qwen-27b-q3")
             llm="qwen/unsloth/Qwen3.8-27B-UD-Q3_K_XL.gguf"
             speculative_type="draft-mtp"
-            executable_args+=(
+            command_args+=(
                 ${qwen_args[@]}
                 -a "Qwen3.8-27B-UD-Q3_K_XL"
                 -c 26624
@@ -146,7 +149,7 @@ autoload() {
             llm="qwen/ista-daslab/Qwen3.8-27B-GSQ-RCO-IQ3_XXS.gguf"
             draft_model="qwen/analogalok/Qwen3.8-27B-DFlash2-Q2_K.gguf"
             speculative_type="draft-dflash"
-            executable_args+=(
+            command_args+=(
                 ${qwen_args[@]}
                 -a "Qwen3.8-27B-GSQ-RCO-IQ3_XXS"
                 -c 65536
@@ -160,6 +163,13 @@ autoload() {
 }
 
 
+kill_processes(){
+    pkill llama
+    pkill qdrant
+    pkill -f ctxpact
+}
+
+
 tools() {
     # Context compaction proxy
     nohup ptyxis -- bash -c "cd $HOME/Documents/github/ctxpact \
@@ -167,7 +177,7 @@ tools() {
         && python -m ctxpact.server --config config-${model}.yaml --local" > "$HOME/temp/nohup-ctxpact.txt" 2>&1 &
     # Embedding model
     embedding_model_path="$HOME/applications/llama-cpp/models/${embedding_model}"
-    nohup ptyxis -- bash -c "${executable} \
+    nohup ptyxis -- bash -c "${command} \
         -m ${embedding_model_path} \
         --rope-scaling yarn \
         --rope-freq-scale .75 \
@@ -198,7 +208,7 @@ main() {
     # Max. batch size for parsing large pfds as text
     #   -b 8192 \
     #   -ub 4096 \
-    executable_args=(
+    command_args=(
         -t 12
         -b 1024
         -ub 512
@@ -213,34 +223,39 @@ main() {
         --host 0.0.0.0
         --port 8080
     )
+    # Kill running processes
+    if [ $restart_flag == true ]; then
+        kill_processes
+    fi
 
     # Load model preferences
     autoload
-    executable_args+=(-m "$HOME/applications/llama-cpp/models/${llm}")
+    command_args+=(-m "$HOME/applications/llama-cpp/models/${llm}")
     if [[ -n $draft_model ]]; then
-        executable_args+=(-md "$HOME/applications/llama-cpp/models/${draft_model}")
+        command_args+=(-md "$HOME/applications/llama-cpp/models/${draft_model}")
     fi
     if [[ -n $multimedia_projector ]]; then
-        executable_args+=(-mm "$HOME/applications/llama-cpp/models/${multimedia_projector}")
+        command_args+=(-mm "$HOME/applications/llama-cpp/models/${multimedia_projector}")
     fi
     if [[ -n $speculative_type ]]; then
-        executable_args+=(--spec-type ${speculative_type})
+        command_args+=(--spec-type ${speculative_type})
     fi
     
     # Act on flags
     if [ $llama_help_flag == true ]; then
-        executable_args=(--help)
-        tools_flags=false
+        command_args=(--help)
+        all_flag=false
     fi
     if [ $verbose_flag == true ]; then
-        executable_args+=(-lv 4)
+        command_args+=(-lv 4)
     fi
-    if [ $tools_flag == true ]; then
+    if [ $all_flag == true ]; then
+        kill_processes
         tools
     fi
 
     # Launch llama.cpp
-    ${executable} ${executable_args[@]}
+    ${command} ${command_args[@]}
 }
 
 
