871453f986b5312bd5c110df611e5cdd5088766b
Blah029 <toransika@gmail.com>
2026-09-15 14:27:43 +0530

    improve argument appending

--- bash/llama-script.sh
+++ bash/llama-script.sh
@@ -10,17 +10,14 @@ Options:
     -h, --help                  Print help and exit
     --llama-help                Print llama.cpp help and exit
     -v, --verbose               Enable verbose output
-    -r, --restart               Kill existing processes before running command
-    -a, --all                   Run all tools for AnythingLLM
+    -a, --all                   Run all supporting tools
     -s, --source <repository>   Binary source. Default github
                                     github          - Run downloaded GitHub Vulkan release
                                     huggingface     - Run HugginFace realease installed from llama.app
     -m, --model <model>         Large language model. Default qwen-27-r
                                     gemma-26b       - Gemma 4 26B A4B
-                                    qwen-27-r       - Empero-AI Qwen 3.8 27B Ridge
-                                    qwen-27b-g-fast - ISTA-DASLab Qwen3.8 27B GSQ RCO GGUF 
-                                    qwen-27b-g-long - ISTA-DASLab Qwen3.8 27B GSQ RCO GGUF 
-    --em <file>                 Vector embedding model in GGUF format. Default nomic/nomic-embed-text-v1.f16.gguf
+                                    qwen-27b-g-fast - ISTA-DASLab Qwen3.8 27B GSQ RCO GGUF IQ3_XXS 
+                                    qwen-27b-g-long - ISTA-DASLab Qwen3.8 27B GSQ RCO GGUF IQ3_S
 EOF
     exit
 }
@@ -37,11 +34,9 @@ parse_arguments() {
     # Defaults
     llama_help_flag=false
     verbose_flag=false
-    restart_flag=false
     all_flag=false
     command_source="huggingface"
     model="qwen-27b-g-fast"
-    embedding_model="nomic/nomic-embed-text-v1.f16.gguf"
     
     # Parse flags and named parameters
     while :; do
@@ -49,7 +44,6 @@ parse_arguments() {
             -h | --help) usage;;
             --llama-help) llama_help_flag=true;;
             -v | --verbose) verbose_flag=true;;
-            -r | --restart) restart_flag=true;;
             -a | --all) all_flag=true;;
             -s | --source)
                 command_source="${2-}"
@@ -57,10 +51,6 @@ parse_arguments() {
                 ;;
             -m | --model)
                 model="${2-}"
-                shift
-                ;;
-            --em)
-                embedding_model="${2-}"
                 shift
                 ;;
             # Exit if an unexpected option is passed
@@ -80,6 +70,38 @@ parse_arguments() {
 }
 
 
+kill_processes(){
+    pkill llama
+    pkill qdrant
+    pkill -f ctxpact
+    sleep 1
+}
+
+
+tools() {
+    # Context compaction proxy
+    nohup ptyxis -- bash -c "cd $HOME/Documents/github/ctxpact \
+        && source .venv/bin/activate \
+        && python -m ctxpact.server --config config-${model}.yaml" > /dev/null 2>&1 &
+    # Embedding model
+    nohup ptyxis -- bash -c "${command} \
+        -a nomic-embed-text-v1 \
+        -m $HOME/applications/llama-cpp/models/nomic/nomic-embed-text-v1.f16.gguf \
+        --rope-scaling yarn \
+        --rope-freq-scale .75 \
+        -c 2048 \
+        -b 2048 \
+        -ub 2048 \
+        -ngl all \
+        --embedding \
+        --host 0.0.0.0 \
+        --port 8081" > /dev/null 2>&1 &
+    # Vector database
+    nohup ptyxis -- bash -c "cd $HOME/applications/qdrant \
+        && ./qdrant" > /dev/null 2>&1 &
+}
+
+
 autoload() {
     # command
     case ${command_source} in
@@ -88,6 +110,7 @@ autoload() {
     esac
 
     # Model
+    model_dir="$HOME/applications/llama-cpp/models/"
     gemma_args=(
         -c 131027
         -ncmoe 7
@@ -115,88 +138,49 @@ autoload() {
         --reasoning-preserve
     )
     case "${model}" in
-        "gemma-26b")
-            llm="gemma/unsloth/gemma-4-26B-A4B-it-qat-UD-Q4_K_XL.gguf"
-            draft_model="gemma/unsloth/mtp-gemma-4-26B-A4B-it-qat-Q8_0.gguf"
-            multimedia_projector="gemma/unsloth/mmproj-gemma-4-26B-A4B-it-qat-F16.gguf"
-            speculative_type="draft-mtp"
-            command_args+=(
-                ${gemma_args[@]}
-                -a "gemma-4-26B-A4B-it-qat-UD-Q4_K_XL"
-            )
-            ;;  
-        "qwen-27b-g-fast")
-            llm="qwen/ista-daslab/Qwen3.8-27B-GSQ-RCO-IQ3_XXS-mtp.gguf"
-            speculative_type="draft-mtp"
-            command_args+=(
-                ${qwen_args[@]}
-                -a "Qwen3.8-27B-GSQ-RCO-IQ3_XXS"
-                -c 106496
-            )
-            ;;
-        "qwen-27b-g-long")
-            llm="qwen/ista-daslab/Qwen3.8-27B-GSQ-RCO-IQ3_S.gguf"
-            draft_model="qwen/hermihg/Qwen3.8-27B-DFlash2-Q2_K_S-MIX.gguf"
-            multimedia_projector="qwen/empero-ai/mmproj-Qwen3.8-27B-BF16.gguf"
-            speculative_type="draft-dflash"
-            command_args+=(
-                ${qwen_args[@]}
-                -a "Qwen3.8-27B-GSQ-RCO-IQ3_S"
-                -c 163840
-                
-            )
-            ;;
+        "gemma-26b") command_args+=(
+            ${gemma_args[@]}
+            -a "gemma-4-26B-A4B-it-qat-UD-Q4_K_XL"
+            -m "${model_dir}/gemma/unsloth/gemma-4-26B-A4B-it-qat-UD-Q4_K_XL.gguf"
+            -md "${model_dir}/gemma/unsloth/mtp-gemma-4-26B-A4B-it-qat-Q8_0.gguf"
+            -mm "${model_dir}/gemma/unsloth/mmproj-gemma-4-26B-A4B-it-qat-F16.gguf"
+            --spec-type "draft-mtp"
+        );;  
+        "qwen-27b-g-fast") command_args+=(
+            ${qwen_args[@]}
+            -a "Qwen3.8-27B-GSQ-RCO-IQ3_XXS"
+            -m "${model_dir}/qwen/ista-daslab/Qwen3.8-27B-GSQ-RCO-IQ3_XXS-mtp.gguf"
+            --spec-type "draft-mtp"
+            -c 106496
+        );;
+        "qwen-27b-g-long") command_args+=(
+            ${qwen_args[@]}
+            -a "Qwen3.8-27B-GSQ-RCO-IQ3_S"
+            -m "${model_dir}/qwen/ista-daslab/Qwen3.8-27B-GSQ-RCO-IQ3_S.gguf"
+            -md "${model_dir}/qwen/hermihg/Qwen3.8-27B-DFlash2-Q2_K_S-MIX.gguf"
+            -mm "${model_dir}/qwen/empero-ai/mmproj-Qwen3.8-27B-BF16.gguf"
+            --spec-type "draft-dflash"
+            -c 163840
+        );;
         *) die "Incorrect model name: ${model}";;
     esac
-}
-
-
-kill_processes(){
-    pkill llama
-    pkill qdrant
-    pkill -f ctxpact
-    sleep 1
-}
-
-
-tools() {
-    # Context compaction proxy
-    nohup ptyxis -- bash -c "cd $HOME/Documents/github/ctxpact \
-        && source .venv/bin/activate \
-        && python -m ctxpact.server --config config-${model}.yaml" > /dev/null 2>&1 &
-    # Embedding model
-    embedding_model_path="$HOME/applications/llama-cpp/models/${embedding_model}"
-    nohup ptyxis -- bash -c "${command} \
-        -m ${embedding_model_path} \
-        --rope-scaling yarn \
-        --rope-freq-scale .75 \
-        -c 2048 \
-        -b 2048 \
-        -ub 2048 \
-        -ngl all \
-        --embedding \
-        --host 0.0.0.0 \
-        --port 8081" > /dev/null 2>&1 &
-    # Vector database
-    nohup ptyxis -- bash -c "cd $HOME/applications/qdrant \
-        && ./qdrant" > /dev/null 2>&1 &
 }
 
 
 main() {
     # Setup comomon parameters
     # Min. batch size for full gpu utilisation
-    #   -b 256 \
-    #   -ub 128 \
+    #   -b 256
+    #   -ub 128
     # Min. batch size for parsing large pdfs as images
-    #   -b 1024 \
-    #   -ub 512 \
+    #   -b 1024
+    #   -ub 512
     # Max. batch size for parsing large pdfs as images 
-    #   -b 2048 \
-    #   -ub 1024 \
+    #   -b 2048
+    #   -ub 1024
     # Max. batch size for parsing large pfds as text
-    #   -b 8192 \
-    #   -ub 4096 \
+    #   -b 8192
+    #   -ub 4096
     command_args=(
         -t 8
         -b 1024
@@ -212,24 +196,9 @@ main() {
         --host 0.0.0.0
         --port 8080
     )
-    # Kill running processes
-    if [ $restart_flag == true ]; then
-        kill_processes
-    fi
 
     # Load model preferences
     autoload
-    command_args+=(-m "$HOME/applications/llama-cpp/models/${llm}")
-    if [[ -n $draft_model ]]; then
-        command_args+=(-md "$HOME/applications/llama-cpp/models/${draft_model}")
-    fi
-    if [[ -n $multimedia_projector ]]; then
-        command_args+=(-mm "$HOME/applications/llama-cpp/models/${multimedia_projector}")
-    fi
-    if [[ -n $speculative_type ]]; then
-        command_args+=(--spec-type ${speculative_type})
-    fi
-    
     # Act on flags
     if [ $llama_help_flag == true ]; then
         command_args=(--help)
@@ -242,7 +211,6 @@ main() {
         kill_processes
         tools
     fi
-
     # Launch llama.cpp
     ${command} ${command_args[@]}
 }
